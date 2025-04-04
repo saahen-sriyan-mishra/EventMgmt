a96674cf80f9acd9b5012fd2ca9d7246df033179
saahen-sriyan-mishra <139043263+saahen-sriyan-mishra@users.noreply.github.com>
2025-04-05 04:36:27 +0530

    Nothing

--- React/vite.config.js
+++ React/vite.config.js
@@ -1,13 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     host:true,
     port: 5173,
-    proxy: {'/api': 'http://localhost:3001'}
+    proxy: {'/api': 'http://localhost:3001'} //Needed due to 1 page layout for multiple users
     
     
   }
